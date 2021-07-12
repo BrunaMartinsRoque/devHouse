3ffaaafe2d48bcc1eedf2a18f7457fb18869e987
Bruna <buhh_martins@hotmail.com>
2021-07-12 20:11:30 -0300

    blog

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res)=>{
-    res.send('Seja Bem vindo');
+    res.sendFile(__dirname + "/views/home.html");
+});
+app.get('/home', (req, res) => {
+    res.redirect('/');
 });
 app.get('/manutencao', (req, res)=>{
 //    res.send('Site em manutenção');
 res.sendFile(__dirname + "/views/manutencao.html")
 });
-
+app.get('/blog', (req, res) => {
+    res.sendFile(__dirname + "/views/blog.html");
+});    
 app.listen(3000);
